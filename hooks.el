61d57b17d0827ed34d1b2955f2e65e11ef27b462
Tobias Gerdin <tger@opera.com>
2009-07-27 09:41:28 +0200

    Moved everything into init.el again.
    Isearch navigation is probably nicer than fiddling with buffers.
    
    Removed mouse-point.el. If I want to do this I should base it on the patch I made to JDEE.

--- hooks.el
+++
@@ -1,8 +0,0 @@
-;;;; Mode hooks go here
-
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda ()
-	    (paredit-mode +1)
-	    (eldoc-mode +1)
-	    (auto-fill-mode +1)
-	    (local-set-key '[C-tab] 'lisp-complete-symbol)))
--- init.el
+++ init.el
@@ -8,12 +8,54 @@
 ;; Add this directory to the load path
 (setq load-path (cons "~/.emacs.d" load-path))
 
-(require 'recentf)
-(ffap-bindings)
+;; (ffap-bindings)
 
-(load "keys.el")
-(load "hooks.el")
-(load "modes.el")
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (line-beginning-position)
+                  (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+
+;;;; Key binding customization goes here
+(global-set-key (quote [f8]) (quote compile)) 
+
+(global-set-key "g" (quote lgrep))
+(global-set-key "G" (quote rgrep))
+(global-set-key "j" (quote imenu))
+(global-set-key "f" (quote find-dired))
+(global-set-key "r" (quote revert-buffer))
+(global-set-key "a" (quote align-regexp))
+(global-set-key "o" (quote occur))
+(global-set-key "c" (quote compile))
+(global-set-key "d" (quote copy-line))
+(global-set-key ";" (quote comment-dwim))
+(global-set-key "t" (quote comment-dwtm))
+
+;; me likes me source indented
+(global-set-key '[remap newline] 'newline-and-indent)
+
+(global-set-key "\C-b" 'Control-X-prefix)
+
+;; (global-set-key "\C-h" 'backward-delete-char)
+(global-set-key (kbd "M-<delete>") 'kill-word)
+
+;;;; Mode hooks go here
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (paredit-mode +1)
+	    (eldoc-mode +1)
+	    (auto-fill-mode +1)
+	    (local-set-key '[C-tab] 'lisp-complete-symbol)))
+
+;;;; Modes that I want enabled by default goes here
+
+;; (iswitchb-mode t)
+(ido-mode t)
+(delete-selection-mode)
+(savehist-mode t)
+(blink-cursor-mode -1)
+(global-hi-lock-mode 1)
 
 ;; paredit
 (autoload 'paredit-mode "paredit"
@@ -27,9 +69,6 @@
      (define-key paredit-mode-map (kbd "M-)")
        'paredit-close-parenthesis-and-newline)))
 
-(defun quick-edit-mode ()
-   (global-set-key '[(mouse-3)] 'yank))
-;(quick-edit-mode)
 
 (setenv "PAGER" "/bin/cat")
 (setenv "EDITOR" "emacsclient")
@@ -51,10 +90,17 @@ Sets the region to the current line and calls `comment-dwim'."
   (end-of-line)
   (comment-dwim nil))
 
-;; Source .emacs if available
-(defvar dot-emacs "~/.tger-emacs")
-(when (and (file-exists-p dot-emacs)
-	   (file-readable-p dot-emacs))
-  (load dot-emacs))
+;; (recentf-mode 1)
+(windmove-default-keybindings)
+;; (filesets-init)
 
+(setq frame-title-format "%f")
+
+;; Source .emacs if available
+(defvar local-dot-emacs "~/.host.el")
+(when (and (file-exists-p local-dot-emacs)
+	   (file-readable-p local-dot-emacs))
+  (load local-dot-emacs))
+
+(setq debug-on-error nil)
 (message "Done.")
